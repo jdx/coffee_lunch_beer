1140cf5cb3abe0f12628921663f0d98304ab15ca
Jeff Dickey <dickeytk@gmail.com>
2012-01-03 14:45:34 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeLunchBeer::Application.config.secret_token = '716c254a33839cfddf0866637d27e0e66c778ee1fc519b93118d908cecbdc54627b78e1adb047cce4a9fd45aaa333906a65b3d4cdc45562761dd540df8636cd8'
